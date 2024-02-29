4a5558ab26a9488a3ef44b8f95098bdba8f6ca80
chrispusonyono <chrispusonyono@gmail.com>
2024-02-29 12:27:58 +0300

    update

--- src/pages/all-doctors.vue
+++ src/pages/all-doctors.vue
@@ -7,35 +7,43 @@
   ></v-data-table-virtual>
 </template>
 
-<script>
-import { fireDb } from '@/utils/constants';
-import { ref, onValue } from "firebase/database";
-    export default {
-      data: ()=>( {
-        
-          headers: [
-            { title: 'Names', align: 'start', key: 'name' },
-            { title: 'Qualifications', align: 'end', key: 'qualification' },
-            { title: 'Experience', align: 'end', key: 'experience' },
-            { title: 'Salaryamount', align: 'end', key: 'salaryamount' },
-            { title: 'Id Number', align: 'end', key: 'number' },
-            { title: 'Employment Date', align: 'end', key: 'year' },
-          ],
-          doctors: [
-            
-          ],
-        
-      }),
-  
-      created() {
-        function fetchDoctors(){
-          onValue(ref(fireDb, '/doctors'), (snapshot) => {
-          snapshot.forEach((doctor) => {
-            this.doctors.push({name:1,qualification:2,experience:3,salaryamount:4,idNumber:5,EmploymentDate:6})
-          })
+<script lang="ts">
+import {fireDb} from '@/utils/constants';
+import {ref, onValue} from "firebase/database";
+
+export default {
+  data: () => ({
+    headers: [
+      {title: 'Names', align: 'start', key: 'name'},
+      {title: 'Qualifications', align: 'end', key: 'qualification'},
+      {title: 'Experience', align: 'end', key: 'experience'},
+      {title: 'Salaryamount', align: 'end', key: 'salaryamount'},
+      {title: 'Id Number', align: 'end', key: 'number'},
+      {title: 'Employment Date', align: 'end', key: 'year'},
+    ],
+    doctors: [] as any,
+  }),
+
+  mounted() {
+
+    this.fetchDoctors();
+  }
+  , methods: {
+    fetchDoctors() {
+      onValue(ref(fireDb, '/doctors'), (snapshot) => {
+        snapshot.forEach((doctor) => {
+          console.log(doctor.val().idNumber)
+          this.doctors.push({
+            name: 1,
+            qualification: 2,
+            experience: 3,
+            salaryamount: doctor.val().salaryamount,
+            idNumber: 'id',
+            EmploymentDate: 6
+          } as any)
         })
-      }
-      fetchDoctors()
-      }
+      })
     }
-  </script>
+  }
+}
+</script>
