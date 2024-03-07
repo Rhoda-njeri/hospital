bb1cc53efd2eb4583169187876c6d1371a788ffe
chrispusonyono <chrispusonyono@gmail.com>
2024-03-07 10:54:43 +0300

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,18 +7,12 @@
 // Composables
 import {createRouter, createWebHistory} from 'vue-router/auto'
 import Dashboard from "@/pages/dashboard.vue";
-import Doctors from "@/pages/doctors.vue";
-import Nurses from "@/pages/nurses.vue";
 
 const routes = [
   {
     path: '/',
     component: Dashboard,
-    children: [
-      {path: '/doctors', component: Doctors},
-      {path: '/doctors', component: Doctors},
-      {path: '/nurses', component: Nurses},
-    ],
+    children: [],
   },
 ]
 const router = createRouter({
