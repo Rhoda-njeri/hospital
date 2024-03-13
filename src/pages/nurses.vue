1ed4b423474470c9110c78609bbaa65f82c95968
chrispusonyono <chrispusonyono@gmail.com>
2024-03-13 10:43:19 +0300

    Fixed save

--- src/pages/nurses.vue
+++ src/pages/nurses.vue
@@ -12,125 +12,124 @@
     </template>
   </v-data-table-virtual>
   <v-dialog
-  <
-  v-model="dialog"
-  max-width="600">
-  <template v-slot:activator="{ props: activatorProps }">
-    <v-btn
-      class="text-none font-weight-regular"
-      prepend-icon="mdi-mother-nurse"
-      text="Add nurse"
-      variant="tonal"
-      v-bind="activatorProps"
-    ></v-btn>
-  </template>
-
-  <v-card
-    prepend-icon="mdi-mother-nurse"
-    title="Nurse Details">
-    <v-card-text>
-      <v-row dense>
-        <v-col
-          cols="12"
-          md="4"
-          sm="6">
-          <v-text-field
-            label="Name"
-            v-model="name"
-            required
-            variant="outlined"
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-          sm="6"
-        >
-          <v-text-field
-            label="Qualification"
-            required
-            v-model="qualification"
-            variant="outlined"
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-          sm="6"
-        >
-          <v-text-field
-            label="Experience"
-            variant="outlined"
-            v-model="experience"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-          sm="6"
-        >
-          <v-text-field
-            label="Salary Amount"
-            required
-            v-model="salaryAmount"
-            variant="outlined"
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-          sm="6"
-        >
-          <v-text-field
-            label="Id Number*"
-            required
-            v-model="idNumber"
-            variant="outlined"
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-          sm="6">
-
-          <v-text-field
-            label="Employment Date*"
-            required
-            v-model="employmentDate"
-            variant="outlined"
-          ></v-text-field>
-        </v-col>
-
-        <small class="text-caption text-decoration-none text-red">{{message}}</small>
-      </v-row>
-    </v-card-text>
-
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <v-spacer></v-spacer>
-
+    v-model="dialog"
+    max-width="600">
+    <template v-slot:activator="{ props: activatorProps }">
       <v-btn
-        text="Close"
-        variant="plain"
-        @click="dialog = false"
-      ></v-btn>
-
-      <v-btn
-        color="primary"
-        text="Save Nurse"
+        class="text-none font-weight-regular"
+        prepend-icon="mdi-mother-nurse"
+        text="Add nurse"
         variant="tonal"
-        :loading="loading"
-        @click="saveNurse"
+        v-bind="activatorProps"
       ></v-btn>
-    </v-card-actions>
-  </v-card>
+    </template>
+
+    <v-card
+      prepend-icon="mdi-mother-nurse"
+      title="Nurse Details">
+      <v-card-text>
+        <v-row dense>
+          <v-col
+            cols="12"
+            md="4"
+            sm="6">
+            <v-text-field
+              label="Name"
+              v-model="name"
+              required
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            sm="6"
+          >
+            <v-text-field
+              label="Qualification"
+              required
+              v-model="qualification"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            sm="6"
+          >
+            <v-text-field
+              label="Experience"
+              variant="outlined"
+              v-model="experience"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            sm="6"
+          >
+            <v-text-field
+              label="Salary Amount"
+              required
+              v-model="salaryAmount"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            sm="6"
+          >
+            <v-text-field
+              label="Id Number*"
+              required
+              v-model="idNumber"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+            sm="6">
+
+            <v-text-field
+              label="Employment Date*"
+              required
+              v-model="employmentDate"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+
+          <small class="text-caption text-decoration-none text-red">{{message}}</small>
+        </v-row>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close"
+          variant="plain"
+          @click="dialog = false"
+        ></v-btn>
+
+        <v-btn
+          color="primary"
+          text="Save Nurse"
+          variant="tonal"
+          :loading="loading"
+          @click="saveNurse"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
   </v-dialog>
 </template>
 
