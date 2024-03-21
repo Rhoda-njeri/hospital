30736124c70986257a26db41020a8884ea178a0e
Rhoda-njeri <thiongorhoda@gmail.com>
2024-03-21 10:57:55 +0300

    added new file

---
+++ src/pages/doctor.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="3">
+          <v-card prepend-icon="mdi-doctor">
+            <template v-slot:title>{{ data }}</template>
+            <v-card-text>Doctors</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+
+        </v-row>
+    </v-container>
+  </template>
+  <script lang="ts">
+  
+  export default {
+    data: () => ({
+        doctor: {},
+        data:"",
+      }
+    ), mounted() {
+      this.fetchData();
+    }
+    , methods: {
+      fetchData() {
+      this.doctor=localStorage.getItem('doctor') as {}
+      this.data = JSON.stringify(this.doctor)
+      }
+    }
+  
+  };
+  </script>
+  <style>
+  /* Your styles go here */
+  </style>
+  
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/doctor': RouteRecordInfo<'/doctor', '/doctor', Record<never, never>, Record<never, never>>,
     '/doctors': RouteRecordInfo<'/doctors', '/doctors', Record<never, never>, Record<never, never>>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
     '/medicines': RouteRecordInfo<'/medicines', '/medicines', Record<never, never>, Record<never, never>>,
